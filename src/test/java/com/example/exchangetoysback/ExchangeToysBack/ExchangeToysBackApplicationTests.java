6d644e0be377ba01c1c128e58cfd8a26d6892710
Matix97 <mateusz_barsnski@outlook.com>
2019-11-23 23:20:19 +0100

    init

---
+++ src/test/java/com/example/exchangetoysback/ExchangeToysBack/ExchangeToysBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangetoysback.ExchangeToysBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeToysBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
